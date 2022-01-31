d4e27e8889d8cb0aea7d1aced63f92d666a03d20
wenanoshe <braiderandreyp@gmail.com>
2022-01-31 16:41:22 -0500

    Making responsive

--- Space-tourism-multipage-website/starter-code/scripts/destination.js
+++ Space-tourism-multipage-website/starter-code/scripts/destination.js
@@ -1,10 +1,13 @@
 const printResult = (name, imageURL, description, distance, travelDuration) => {
+    /* This functions recives for parameters the values inside each element in the html file */
+    // Getting the elements by class and ids
     const e_destinyName = document.querySelector('.destination__destination');
     const e_img = document.querySelector('.destination__img');
     const e_description = document.querySelector('.destination__description');
     const e_distance = document.getElementById('distance');
     const e_travel = document.getElementById('travel');
 
+    // Asign their values
     e_destinyName.textContent = name;
     e_img.setAttribute('src', imageURL);
     e_description.textContent = description;
@@ -13,12 +16,13 @@ const printResult = (name, imageURL, description, distance, travelDuration) => {
 }
 
 const getDestination = async(clickedElement) => {
+    /* This function make the request and  extract the Desination object*/
     const request = await fetch('data.json');
     const result = await request.json();
     const destinations = result.destinations;
 
     destinations.filter(destiny => {
-        if(destiny.name == clickedElement) {
+        if(destiny.name == clickedElement.name) {
             printResult(destiny.name, destiny.images.webp, destiny.description, destiny.distance, destiny.travel);
         }
     });
@@ -27,49 +31,40 @@ const getDestination = async(clickedElement) => {
 }
 
 const decorateActivePage = () => {
-    let destinyTitle = document.querySelector('.destination__destination');
+    let destinyTitle = document.querySelector('.destination__destination').textContent;
     
     for (const child of ul.children) {
-        if(child.children[0].classList.contains('active-link')) {
-            child.children[0].classList.remove('active-link');
-        } else {
-            switch (destinyTitle.textContent) {
-                case 'Moon':
-                    ul.children[0].children[0].classList.add('active-link');
-                    break;
-                case 'Mars':
-                    ul.children[1].children[0].classList.add('active-link');
-                    break;
-            
-                case 'Europa': 
-                    ul.children[2].children[0].classList.add('active-link');
-                    break;
-            
-                case 'Titan': 
-                    ul.children[3].children[0].classList.add('active-link');
-                    break;
-            
-                default:
-                    console.log('error');
-                    break;
-            }
-        }
+        child.children[0].classList.remove('active-link');
     }
+    switch (destinyTitle) {
+        case 'Moon':
+            ul.children[0].children[0].classList.add('active-link');
+            break;
+        case 'Mars':
+            ul.children[1].children[0].classList.add('active-link');
+            break;
     
+        case 'Europa': 
+            ul.children[2].children[0].classList.add('active-link');
+            break;
     
+        case 'Titan': 
+            ul.children[3].children[0].classList.add('active-link');
+            break;
+    
+        default:
+            console.log('error');
+            break;
+    }
 }
 
 
 const ul = document.querySelector('.destination__ul');
 
-let previousTargetedElement;
-ul.addEventListener('click', (e) => {
-    previousTargetedElement = e.target;
-    getDestination(e.target.name);
-});
-
-decorateActivePage();
-
-
+for (const tab of ul.children) {
+    tab.addEventListener('click', (e) => {
+        getDestination(e.target);
+    });    
+}
 
 
