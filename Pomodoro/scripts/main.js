f596227abf3b5f5323807b7b519bbaffa5fb7530
wenanoshe <braiderandreyp@gmail.com>
2022-02-09 15:54:54 -0500

    Finished all modes (pomodoro, short and long break) but with some errors

--- Pomodoro/scripts/main.js
+++ Pomodoro/scripts/main.js
@@ -41,18 +41,45 @@ const runTimer = () => {
 // ---- Handdle the settings (modal window) ----
 const showSettings = () => document.querySelector('.modal').classList.toggle('show-modal');
 
-const getPomodoroValues = () => {
-   // This is for get the values of the pomo short and long break
-   currentPomo = document.getElementById('pomodoro').value * 60; 
-   currentShortBreak = document.getElementById('short-break').value * 60;
-   currentLongBreak = document.getElementById('long-break').value * 60;
-   applyChangesInInterface();
-}
+// const getPomodoroValues = () => {
+//    // This is for get the values of the pomo short and long break
+//    currentPomo = document.getElementById('modal--setPomodoro').value * 60; 
+//    // currentPomo = document.getElementById('modal--setShort-break').value * 60;
+//    // currentPomo = document.getElementById('modal--setLong-break').value * 60;
+//    applyChangesInInterface();
+// }
 
 const applyChangesInInterface = () => {
-   minutesEl.textContent = document.getElementById('pomodoro').value;
-   // minutesEl.textContent = document.getElementById('pomodoro').value;
-   // minutesEl.textContent = document.getElementById('pomodoro').value;
+   // We need to verify in which mode we are and insert that values in the interface
+   let modesCollection = []
+
+   // With this loop we add the btn element to the modesCollection array
+   for (const elem of modesBar.children) {modesCollection.push(elem.children[0])} 
+
+   // Now, we filter the modesCollection array to get that has the class *active-mode*
+   let activeMode = modesCollection.filter(i => {
+      if(i.classList.contains('active-mode')) {return i};
+   });
+
+   // Here we access to the active element and depending on the id of the element we made the insertion of the minutes that we set in the
+   const modalSetPomodoro = document.getElementById('modal--setPomodoro').value;
+   const modalSetShortbreak = document.getElementById('modal--setShort-break').value;
+   const modalSetLongBreak = document.getElementById('modal--setLong-break').value;
+
+   switch (activeMode[0].id) {
+      case 'mode-pomodoro':
+         minutesEl.textContent = modalSetPomodoro;
+         currentPomo = modalSetPomodoro * 60;
+      break;
+      case 'mode-shortBreak':
+         minutesEl.textContent = modalSetShortbreak;
+         currentPomo = modalSetShortbreak * 60;
+      break;
+      case 'mode-longBreak':
+         minutesEl.textContent = modalSetLongBreak;
+         currentPomo = modalSetLongBreak * 60;
+      break;
+   }
 }
 
 /* === === === */
@@ -73,46 +100,44 @@ let applySettingsBtn = document.getElementById('applyChanges');
 let minutesEl = document.querySelector('.pomo__minutes');
 let secondsEl = document.querySelector('.pomo__seconds');
 
+const modesBar = document.querySelector('.modes__ul');
+
+let settingsBtn = document.querySelector('.settings__btn');
+let closeModalBtn = document.querySelector('.modal__close');
+
 /* === === === */
 
 
 /* === Code execution === */
 
 // ---- show and hide the modal ----
-   let settingsBtn = document.querySelector('.settings__btn');
-   let closeModalBtn = document.querySelector('.modal__close');
+settingsBtn.addEventListener('click', showSettings);
+closeModalBtn.addEventListener('click', showSettings);
 
-   settingsBtn.addEventListener('click', showSettings);
-   closeModalBtn.addEventListener('click', showSettings);
-// ---- --- ----
-
-formEl.addEventListener('submit', e => e.preventDefault());
-getPomodoroValues();
-applySettingsBtn.addEventListener('click', getPomodoroValues);
+// Reset the form
+formEl.addEventListener('submit', e => e.preventDefault()); 
+applySettingsBtn.addEventListener('click', applyChangesInInterface);
 applySettingsBtn.addEventListener('click', showSettings);
 
-// When starts the pomodoro
+// I load this funcito instead the page loads for stores the values
+applyChangesInInterface();
+
+// ---- When starts the pomodoro ----
 pomodoroEl.addEventListener('click', runTimer);
 
+// ---- changing between modes (pomo and breaks) ----
+modesBar.addEventListener('click', e => {
+   if(e.target && e.target.tagName == 'BUTTON') {
+      // First I remove the active state of the element that has it
+      for (const child of modesBar.children) {
+         child.children[0].classList.remove('active-mode');
+      }
+      // After I adds the active state of the clicked element
+      e.target.classList.add('active-mode');
+
+      // Update the values in interface
+      applyChangesInInterface()
+   }
+})
+
 /* === === === */
-
-
-// let inter;
-// let plus = 0;
-// let sstatus = false
-// const inn = () => {
-//    sstatus = true;
-//    if(sstatus) {
-//       inter = setInterval(() => {
-//          plus += 1;
-         
-//       console.log(plus);
-//       if(plus == 5) sstatus = false;
-//       if(sstatus == false)
-
-//       }, 100);
-//    }
-// }
-
-
-// inn();
