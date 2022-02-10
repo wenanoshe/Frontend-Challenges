4a9750f2d5745f6658d1a92c8aded30dcad87998
wenanoshe <braiderandreyp@gmail.com>
2022-02-10 18:51:15 -0500

    Verifications...

--- Pomodoro/scripts/main.js
+++ Pomodoro/scripts/main.js
@@ -2,11 +2,44 @@
 
 /* === Functions === */
 
+const verifyFormFields = (pomo_value, short_value, long_value) => {
+   // This promise is for verify if the user not set invalid numbers in the settings
+   return new Promise((resolve, reject) => {
+      if(pomo_value <= 0 || short_value <= 0 || long_value <= 0) {
+         reject(alert('Numbers less than 0 are not allowed!!'));
+      } 
+      else if(pomo_value.length == 0 || short_value.length == 0 || long_value.length == 0) {
+         reject(alert('Empty values are not allowed!!'));
+      }
+      else {
+         resolve('applyChangesInterface is executed succesful');
+      }
+   });
+}
+
+
+const playSound = () => {
+   // Play a sound when starts the pomodoro
+   document.getElementById('bellSong').play();
+}
+
+const verifyChangeOfMode = (valueToStoreinMinutes) => {
+   stopInterval();
+   minutesEl.textContent = valueToStoreinMinutes;
+   currentPomo = valueToStoreinMinutes * 60;
+}
+
+const stopInterval = () => {
+   // Function used to stop the interval
+   clearInterval(interval);
+   interval = null;
+}
+
 const updateInterfaceTimer = (min, sec) => {
    // Function that update the interface timer
    minutesEl = document.querySelector('.pomo__minutes');
    secondsEl = document.querySelector('.pomo__seconds');
-
+   
    minutesEl.textContent = min.toString().padStart(2, "0");
    secondsEl.textContent = sec.toString().padStart(2, "0");
 }
@@ -15,13 +48,18 @@ const countdown = () => {
    // This is the interval that starts the count down
    if(!interval) {
       interval = setInterval(() => {
-      let minutes = Math.floor(currentPomo / 60);
-      let seconds = currentPomo % 60;
-      
-      currentPomo = currentPomo - 1;
-   
-      updateInterfaceTimer(minutes, seconds);
-   }, 1000);   
+         // Verfiy if the interval has not finished
+         if(currentPomo == -1) {
+            stopInterval();
+         } else {
+            let minutes = Math.floor(currentPomo / 60);
+            let seconds = currentPomo % 60;
+         
+            currentPomo = currentPomo - 1;
+         
+            updateInterfaceTimer(minutes, seconds);
+         }
+      }, 1000);   
    }
 }
 
@@ -29,57 +67,55 @@ const runTimer = () => {
    let statusEl = document.getElementById('status');
    if(statusEl.classList.contains('fi-br-play')){
       statusEl.classList.replace('fi-br-play', 'fi-br-pause');
-
+      
       countdown();
+      playSound();
    } else {
       statusEl.classList.replace( 'fi-br-pause', 'fi-br-play');
-      clearInterval(interval);
-      interval = null;
+
+      stopInterval();
+      playSound();
    }
 }
 
 // ---- Handdle the settings (modal window) ----
 const showSettings = () => document.querySelector('.modal').classList.toggle('show-modal');
 
-// const getPomodoroValues = () => {
-//    // This is for get the values of the pomo short and long break
-//    currentPomo = document.getElementById('modal--setPomodoro').value * 60; 
-//    // currentPomo = document.getElementById('modal--setShort-break').value * 60;
-//    // currentPomo = document.getElementById('modal--setLong-break').value * 60;
-//    applyChangesInInterface();
-// }
-
 const applyChangesInInterface = () => {
    // We need to verify in which mode we are and insert that values in the interface
    let modesCollection = []
-
+   
    // With this loop we add the btn element to the modesCollection array
    for (const elem of modesBar.children) {modesCollection.push(elem.children[0])} 
-
+   
    // Now, we filter the modesCollection array to get that has the class *active-mode*
    let activeMode = modesCollection.filter(i => {
       if(i.classList.contains('active-mode')) {return i};
    });
-
+   
    // Here we access to the active element and depending on the id of the element we made the insertion of the minutes that we set in the
    const modalSetPomodoro = document.getElementById('modal--setPomodoro').value;
    const modalSetShortbreak = document.getElementById('modal--setShort-break').value;
    const modalSetLongBreak = document.getElementById('modal--setLong-break').value;
-
-   switch (activeMode[0].id) {
-      case 'mode-pomodoro':
-         minutesEl.textContent = modalSetPomodoro;
-         currentPomo = modalSetPomodoro * 60;
-      break;
-      case 'mode-shortBreak':
-         minutesEl.textContent = modalSetShortbreak;
-         currentPomo = modalSetShortbreak * 60;
-      break;
-      case 'mode-longBreak':
-         minutesEl.textContent = modalSetLongBreak;
-         currentPomo = modalSetLongBreak * 60;
-      break;
-   }
+   
+   verifyFormFields(modalSetPomodoro, modalSetShortbreak, modalSetLongBreak)
+   .then(fullfiled => {
+      console.log(fullfiled);
+      switch (activeMode[0].id) {
+         case 'mode-pomodoro':
+            verifyChangeOfMode(modalSetPomodoro);
+         break;
+   
+         case 'mode-shortBreak':
+            verifyChangeOfMode(modalSetShortbreak);
+         break;
+         
+         case 'mode-longBreak':
+            verifyChangeOfMode(modalSetLongBreak)
+         break;
+      }
+   })
+   .catch(err => err);
 }
 
 /* === === === */
@@ -134,10 +170,11 @@ modesBar.addEventListener('click', e => {
       }
       // After I adds the active state of the clicked element
       e.target.classList.add('active-mode');
-
+      
       // Update the values in interface
       applyChangesInInterface()
    }
 })
 
 /* === === === */
+
