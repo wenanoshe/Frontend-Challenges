d079a201e0820cb4b9fa306749c8668b3686255f
wenanoshe <braiderandreyp@gmail.com>
2022-03-03 14:20:16 -0500

    Changes to the demo page

--- simpleCountDown/scripts/demo.js
+++ simpleCountDown/scripts/demo.js
@@ -1,12 +1,26 @@
+'use-strict';
+
 import { Countdown } from "./countdown.js";
 
 const pause = document.getElementById('pause');
 const play = document.getElementById('play');
 const reload = document.getElementById('reload');
-
 const timeTarget = document.getElementById('time');
+const initValue = document.getElementById('initValue');
 
-let demo = new Countdown(timeTarget, 5000, true);
+let demo;
+
+demo = new Countdown(timeTarget, initValue.defaultValue);
+
+if(!typeof initValue.value == "number") {
+   console.log('nices');
+} else console.log(initValue.value);
+
+initValue.addEventListener('keyup', () => {
+   demo = new Countdown(timeTarget, initValue.value);
+   console.log(initValue.value);
+});
+
 
 pause.addEventListener('click', demo.stop);
 play.addEventListener('click', demo.play);
